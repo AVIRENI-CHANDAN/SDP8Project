522229791dcd2786c7df2490e6534236554c46ce
Avireni Chandan <avireni.chandanchandu43657@gmail.com>
2020-12-11 15:44:23 +0530

    Project upto farmer pages

--- FarmerSellingSection.js
+++ FarmerSellingSection.js
@@ -15,7 +15,7 @@ function calculateThePrice() {
     // The below is the equiuvlanet of the above two statements
     (document.getElementById("theCropSelected")).innerHTML = (document.getElementsByTagName("option")[ x ].text);
     var valueOfTheUnitCrop = (document.getElementById("theCropUnitPrice"));
-    valueOfTheUnitCrop.innerHTML = (cropPrices[indvalCropSelected] + " ₹").toString();
+    valueOfTheUnitCrop.innerHTML = (cropPrices[ indvalCropSelected ] + " ₹").toString();
     // console.log(valueOfTheUnitCrop.innerHTML);
     var inputWeight = document.getElementById("yieldWeight").value;
     // console.log(inputWeight);
@@ -26,7 +26,7 @@ function calculateThePrice() {
     var priceOfYield = cropPrices[ indvalCropSelected ] * inputWeight;
     var amountinwords = inWords(Math.floor(priceOfYield));
     // console.log(amountinwords);
-    (document.getElementById("theCalculatedCropPrice")).innerHTML = numberWithCommas(priceOfYield.toFixed(2));
+    (document.getElementById("theCalculatedCropPrice")).innerHTML = numberWithCommas(priceOfYield.toFixed(2)) + " ₹";
     (document.getElementById("theCalculatedCropPriceInWords")).innerHTML = amountinwords;
 }
 function numberWithCommas(x) {
@@ -35,17 +35,17 @@ function numberWithCommas(x) {
 
 // To convert to words
 
-var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
-var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+var a = [ '', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen ' ];
+var b = [ '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
 
-function inWords (num) {
+function inWords(num) {
     if ((num = num.toString()).length > 9) return 'overflow';
     n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
     if (!n) return; var str = '';
-    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
-    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
-    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
-    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
-    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'only ' : '';
+    str += (n[ 1 ] != 0) ? (a[ Number(n[ 1 ]) ] || b[ n[ 1 ][ 0 ] ] + ' ' + a[ n[ 1 ][ 1 ] ]) + 'crore ' : '';
+    str += (n[ 2 ] != 0) ? (a[ Number(n[ 2 ]) ] || b[ n[ 2 ][ 0 ] ] + ' ' + a[ n[ 2 ][ 1 ] ]) + 'lakh ' : '';
+    str += (n[ 3 ] != 0) ? (a[ Number(n[ 3 ]) ] || b[ n[ 3 ][ 0 ] ] + ' ' + a[ n[ 3 ][ 1 ] ]) + 'thousand ' : '';
+    str += (n[ 4 ] != 0) ? (a[ Number(n[ 4 ]) ] || b[ n[ 4 ][ 0 ] ] + ' ' + a[ n[ 4 ][ 1 ] ]) + 'hundred ' : '';
+    str += (n[ 5 ] != 0) ? ((str != '') ? 'and ' : '') + (a[ Number(n[ 5 ]) ] || b[ n[ 5 ][ 0 ] ] + ' ' + a[ n[ 5 ][ 1 ] ]) + 'only ' : '';
     return str;
 }
